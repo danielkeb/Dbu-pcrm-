c2748d3fe6ec98fe8343c53eebdb430381b17c13
Daniel <daniel@kali.kali>
2024-06-27 08:13:00 -0400

    complete

--- dbu/src/seeder/seeder.service.ts
+++ dbu/src/seeder/seeder.service.ts
@@ -1,20 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service'; // Adjust the import path
+import * as argon from 'argon2';
 
 @Injectable()
 export class SeederService {
+  private readonly logger = new Logger(SeederService.name);
+
   constructor(private prisma: PrismaService) {}
 
   async seed() {
-    await this.prisma.users.create({
-      data: {
-        id: 438,
-        name: 'Daniel',
-        email: 'danielkebede3811@gmail.com',
-        role: 'admin',
-        password: 'admin1234',
-      },
-    });
-    return { msg: 'success' };
+    try {
+      const hash = await argon.hash('admin1234');
+      const existingUser = await this.prisma.users.findUnique({
+        where: { email: 'john@gmail.com' },
+      });
+
+      if (!existingUser) {
+        await this.prisma.users.create({
+          data: {
+            id:8743,
+            name: 'John',
+            email: 'johnmike@gmail.com',
+            role: 'admin',
+            last_name: 'mike',
+            address: 'Dbu',
+            gender: 'Male',
+            status: 'active',
+            phonenumer: '09216534',
+            password: hash,
+          },
+        });
+      }
+
+      return { msg: 'success' };
+    } catch (error) {
+      this.logger.error(`Seeding failed: ${error.message}`);
+      throw new Error(`Seeding failed: ${error.message}`);
+    }
   }
 }
